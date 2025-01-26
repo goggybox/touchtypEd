c574393fb8248e480df7920cb79a4b8ad2bc0f5b
YuhangN <s2269587@ed.ac.uk>
2025-01-26 23:46:02 +0000

    1. Add navigation bar to game view with identical layout to learn view 2. Add click handler for learn button in game view 3. Set correct button states (selected/unselected) in navigation bar
    By Yuhang and Owen. 1.26

--- src/main/java/com/example/touchtyped/controller/GameViewController.java
+++ src/main/java/com/example/touchtyped/controller/GameViewController.java
@@ -4,19 +4,38 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.VBox;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class GameViewController {
     @FXML
-    private VBox gameContainer;
-    
+    private ImageView learnButton;
+
     @FXML
-    private void onBackButtonClick() throws IOException {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/touchtyped/learn-view.fxml"));
-        Scene scene = new Scene(loader.load(), 1200, 700);
-        Stage stage = (Stage) gameContainer.getScene().getWindow();
-        stage.setScene(scene);
+    private ImageView gamesButton;
+
+    @FXML
+    private ImageView optionsButton;
+
+    @FXML
+    private VBox gameContainer;
+
+    @FXML
+    public void onLearnButtonClick() {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/touchtyped/learn-view.fxml"));
+            Scene scene = new Scene(loader.load(), 1200, 700);
+            Stage stage = (Stage) learnButton.getScene().getWindow();
+            stage.setScene(scene);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    public void onOptionsButtonClick() {
+        System.out.println("Options button clicked!");
     }
 } 
