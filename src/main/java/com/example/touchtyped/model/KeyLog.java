5f563938f7d66bfe1304df5666d0ef87d0bc5528
Georgeinio <gibedford04@gmail.com>
2025-01-27 23:08:33 +0000

    begin KeyLogsStructure and KeyLog

---
+++ src/main/java/com/example/touchtyped/model/KeyLog.java
@@ -0,0 +1,56 @@
+package com.example.touchtyped.model;
+
+public class KeyLog {
+
+    private String key;
+    private String expected;
+    private long timestamp;
+    private boolean error;
+
+    /**
+     * constructor
+     */
+    public KeyLog(String key, String expected, long timestamp, boolean error) {
+        this.key = key;
+        this.expected = expected;
+        this.timestamp = timestamp;
+        this.error = error;
+    }
+
+
+    /**
+     * Getters and setters
+     */
+    public boolean isError() {
+        return error;
+    }
+
+    public void setError(boolean error) {
+        this.error = error;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getExpected() {
+        return expected;
+    }
+
+    public void setExpected(String expected) {
+        this.expected = expected;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+}
