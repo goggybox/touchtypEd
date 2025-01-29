0e781e1859d2fcf96db63a3ed39c6f0f9863752e
Georgeinio <gibedford04@gmail.com>
2025-01-29 17:07:39 +0000

    implemented module page and clicking Module buttons to go to module page

--- src/main/java/com/example/touchtyped/controller/ModuleViewController.java
+++ src/main/java/com/example/touchtyped/controller/ModuleViewController.java
@@ -1,34 +1,92 @@
 package com.example.touchtyped.controller;
 
+import com.example.touchtyped.constants.StyleConstants;
 import com.example.touchtyped.model.Module;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class ModuleViewController {
 
-    @FXML
-    private Label moduleName;
 
     @FXML
-    private Label moduleFocus;
+    private HBox charContainer;
 
     @FXML
-    private Label moduleLevels;
+    private Label moduleDisplayText;
 
-    private Stage stage;
+    /**
+     * reference to the gamesButton in learn-view.fxml
+     */
+    @FXML
+    private ImageView gamesButton;
+
+    @FXML
+    private Region spacer;
+
+    @FXML
+    private Label closeButton;
+
+    public void initialize() {
+        moduleDisplayText.setStyle(String.format("-fx-text-fill: %s; -fx-font-size: 36px; " +
+                "-fx-font-family: 'Antipasto'; -fx-padding: 40px 0px 0px 80px;", StyleConstants.GREY_COLOUR));
+
+        closeButton.setStyle(String.format("-fx-font-size: 20px; -fx-cursor: hand; -fx-text-fill: %s;" +
+                " -fx-padding: 40px 80px 0px 0px", StyleConstants.GREY_COLOUR));
+    }
 
     public void setModule(Module module) {
-        moduleName.setText(module.getName());
-        moduleFocus.setText("Focus: " + module.getFocus());
+        moduleDisplayText.setText(module.getDisplayText());
 
-        StringBuilder levelsText = new StringBuilder("Levels:\n");
-        module.getLevels().forEach(level -> levelsText.append("- ").append(level.getTaskString()).append("\n"));
-        moduleLevels.setText(levelsText.toString());
+
+        charContainer.getChildren().clear();
+
+        if (!module.getLevels().isEmpty()) {
+            String firstLevelText = module.getLevels().get(0).getTaskString();
+
+            for (char c : firstLevelText.toCharArray()) {
+                StackPane letterBox = createLetterBox(c);
+                charContainer.getChildren().add(letterBox);
+            }
+        }
+    }
+
+    private StackPane createLetterBox(char c) {
+        Rectangle box = new Rectangle(50, 50);
+        box.setArcWidth(30);
+        box.setArcHeight(30);
+        box.setStroke(Color.web(StyleConstants.GREY_COLOUR));
+        box.setStrokeWidth(4);
+        box.setFill(Color.TRANSPARENT);
+
+        Label letter = new Label(String.valueOf(c));
+        letter.setStyle(String.format("-fx-font-size: 32px; -fx-font-family: 'Manjari'; -fx-text-fill: %s", StyleConstants.GREY_COLOUR));
+
+        StackPane container = new StackPane();
+        container.getChildren().addAll(box, letter);
+        return container;
+    }
+
+    @FXML
+    public void onGamesButtonClick() {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/touchtyped/game-view.fxml"));
+            Scene scene = new Scene(loader.load(), 1200, 700);
+            Stage stage = (Stage) gamesButton.getScene().getWindow();
+            stage.setScene(scene);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @FXML
@@ -36,7 +94,7 @@ public class ModuleViewController {
         try {
             FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/touchtyped/learn-view.fxml"));
             Scene scene = new Scene(loader.load(), 1200, 700);
-            Stage stage = (Stage) moduleName.getScene().getWindow();
+            Stage stage = (Stage) gamesButton.getScene().getWindow();
             stage.setScene(scene);
         } catch (IOException e) {
             e.printStackTrace();
