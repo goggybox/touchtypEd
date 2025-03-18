c239bc60e97a9e81a42b2d3bd041d1a3bac9e4af
yuanchaoyang <s2279011@ed.ac.uk>
2025-03-18 15:33:17 +0000

    update module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,6 +4,9 @@ module com.example.touchtyped {
     requires javafx.base;
     requires javafx.graphics;
     
+    // Java AWT/Swing
+    requires java.desktop;
+    
     // Jackson JSON
     requires com.fasterxml.jackson.databind;
     requires com.fasterxml.jackson.core;
@@ -20,21 +23,31 @@ module com.example.touchtyped {
     requires spring.tx;
 
     // JPA
-    requires java.persistence;
+    // 移除旧版JPA依赖，使用jakarta.persistence
+    // requires java.persistence;
     
     // HTTP Server
     requires jdk.httpserver;
     requires spring.webmvc;
     requires spring.data.jpa;
     requires jakarta.persistence;
+    requires com.fazecast.jSerialComm;
 
     // 开放包供Spring反射访问
     opens com.example.touchtyped.model to spring.core, spring.beans, com.fasterxml.jackson.databind;
     opens com.example.touchtyped.server to spring.core, spring.beans, spring.context, spring.web;
     opens com.example.touchtyped.service to spring.core, spring.beans;
     
+    // 开放控制器包供JavaFX FXML加载器访问
+    opens com.example.touchtyped.controller to javafx.fxml;
+    
+    // 开放序列化器包供Jackson访问
+    opens com.example.touchtyped.serialisers to com.fasterxml.jackson.databind;
+    
     exports com.example.touchtyped.app;
     exports com.example.touchtyped.model;
     exports com.example.touchtyped.service;
     exports com.example.touchtyped.server;
+    exports com.example.touchtyped.controller;
+    exports com.example.touchtyped.serialisers;
 }
