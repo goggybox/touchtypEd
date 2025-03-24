9666490b68a47ec2a5719ac8b102b0a82345dcbb
yuanchaoyang <s2279011@ed.ac.uk>
2025-03-24 14:04:49 +0000

    commit agagin

--- src/main/java/com/example/touchtyped/controller/DifficultyViewController.java
+++ src/main/java/com/example/touchtyped/controller/DifficultyViewController.java
@@ -2,63 +2,58 @@ package com.example.touchtyped.controller;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-/**
- * Controller for the difficulty selection view
- */
 public class DifficultyViewController {
-    // This VBox corresponds to the root node in difficulty-view.fxml (fx:id="root")
-    // If you need to get the current Stage, you can do so with root.getScene().getWindow()
+
     @FXML
-    private BorderPane rootPane; // Corresponds to fx:id="rootPane"
-    
-    // When 15s button is clicked
+    private BorderPane rootPane;
+
     @FXML
-    private void on15sButtonClick() {
-        loadGameViewWithTime(15);
+    public void on15sSelect() {
+//        Stage stage = (Stage) rootPane.getScene().getWindow();
+        openGameWithTime(15);
     }
-    
-    // When 30s button is clicked
+
     @FXML
-    private void on30sButtonClick() {
-        loadGameViewWithTime(30);
+    public void on30sSelect() {
+        openGameWithTime(30);
     }
-    
-    // When 60s button is clicked
+
+
     @FXML
-    private void on60sButtonClick() {
-        loadGameViewWithTime(60);
+    public void on60sSelect() {
+        openGameWithTime(60);
     }
-    
-    // When 120s button is clicked
+
     @FXML
-    private void on120sButtonClick() {
-        loadGameViewWithTime(120);
+    public void on120sSelect() {
+        openGameWithTime(120);
     }
-    
-    /**
-     * Generic method: After selecting difficulty, load GameView with time parameter
-     * @param time Seconds to set (15/30/60/120)
-     */
-    private void loadGameViewWithTime(int time) {
+
+    private void openGameWithTime(int time) {
         try {
-            // 1. Load GameView FXML
             FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/touchtyped/game-view.fxml"));
-            Scene gameScene = new Scene(loader.load(), 1200, 700);
-            
-            // 2. Get the corresponding GameViewController
-            GameViewController controller = loader.getController();
-            // You can directly call the logic for the 4 buttons here
-            controller.initTimedGame(time);
-            
-            // 3. Set the scene to the current stage
+            Parent gameRoot = loader.load();
+
+            GameViewController gameController = loader.getController();
+
+            switch (time) {
+                case 15 -> gameController.select15s();
+                case 30 -> gameController.select30s();
+                case 60 -> gameController.select60s();
+                case 120 -> gameController.select120s();
+            }
+
             Stage stage = (Stage) rootPane.getScene().getWindow();
-            stage.setScene(gameScene);
+            stage.setScene(new Scene(gameRoot, 1200, 700));
+
         } catch (IOException e) {
             e.printStackTrace();
         }
